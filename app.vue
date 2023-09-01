6abd791fdacdd91fb3b4709daad5f1600098fb51
Qaudri <aquadirmuhammad@gmail.com>
2023-09-01 12:23:44 +0100

    chore: app.vue updated to match main website

--- app.vue
+++ app.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
-    <NuxtPage/>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@500;600;700;800&display=swap');
+html {
+  scroll-behavior: smooth;
+  -webkit-overflow-scrolling: touch;
+}
+
+body {
+  overflow-y: visible;
+	position: relative;
+}
+
+* {
+  font-family: 'Open Sans' sans-serif;
+  box-sizing: border-box;
+}
+
+*::-webkit-scrollbar {
+  width:0px;
+  cursor: pointer;
+}
+</style>
