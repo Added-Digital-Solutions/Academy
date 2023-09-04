56332ff0e729e7757d8a4901879788b25ace646a
Qaudri <aquadirmuhammad@gmail.com>
2023-09-04 12:09:18 +0100

    chore: route links filled in navbar component

--- components/Navigation/NavBar.vue
+++ components/Navigation/NavBar.vue
@@ -10,16 +10,16 @@
       <nav :class="menu_revealed ? 'translate-x-0' : '-translate-x-full'" class="md:h-fit z-100 md:static md:bg-transparent w-full md:w-fit fixed h-screen left-0 top-[65.5px] md:top duration-150 ease-in-out md:translate-x-0 bg-white">
         <ul class="grid grid-cols-1 place-items-center md:flex gap-5 md:gap-3 lg:gap-5 items-center px-6 pt-20 md:p-0">
           <li>
-            <UiButtonsTertiary>Home</UiButtonsTertiary>
+            <UiButtonsTertiary @clicked="$router.push({name: 'index'})">Home</UiButtonsTertiary>
           </li>
           <li>
-            <UiButtonsTertiary @clicked="$router.push({path: '/courses'})">Courses</UiButtonsTertiary>
+            <UiButtonsTertiary @clicked="$router.push({name: 'courses'})">Courses</UiButtonsTertiary>
           </li>
           <li>
-            <UiButtonsTertiary>Blog</UiButtonsTertiary>
+            <UiButtonsTertiary @clicked="$router.push({name: 'blog'})">Blog</UiButtonsTertiary>
           </li>
           <li>
-            <UiButtonsTertiary>Login</UiButtonsTertiary>
+            <UiButtonsTertiary @clicked="$router.push({name: 'login'})">Login</UiButtonsTertiary>
           </li>
           <li>
             <UiButtonsPrimary @clicked="$router.push({path: '/signup'})">Sign Up</UiButtonsPrimary>
