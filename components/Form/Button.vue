851f37d248ffd2dce46a73473d35b1319f8fe0b8
Qaudri <aquadirmuhammad@gmail.com>
2023-09-04 13:12:41 +0100

    refactor: form button component updated to match colors

--- components/Form/Button.vue
+++ components/Form/Button.vue
@@ -1,8 +1,8 @@
 <template>
   <button
     type="submit"
-    class="py-2 px-5 flex w-full text-white font-medium text-base 2xl:text-lg justify-center duration-500 ease-in-out rounded-md focus:bg-primary " 
-    :disabled="loading || disabled" :class="loading ? 'bg-primary': 'bg-secondary hover:bg-primary'">
+    class="py-2 px-5 flex w-full text-white font-medium text-base 2xl:text-lg justify-center duration-500 ease-in-out rounded-md focus:bg-secondary " 
+    :disabled="loading || disabled" :class="loading ? 'bg-secondary': 'bg-primary hover:bg-secondary'">
 
     <div class="flex items-center justify-center uppercase">
       <div v-if="loading"  >
