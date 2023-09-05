c6ee8261a60833a60351aae9ed947b7fe873d55f
Qaudri <aquadirmuhammad@gmail.com>
2023-09-05 09:34:24 +0100

    refactor: footer design updated

--- components/Navigation/Footer.vue
+++ components/Navigation/Footer.vue
@@ -1,10 +1,28 @@
 <template>
   <footer class="bg-accent border-t border-white text-white">
-    <div class="max-w-7xl container mx-auto px-6 pt-20 pb-8 flex justify-between items-center">
-      <div class="w-36" >
-        <NavigationLogo />
+    <div class="max-w-7xl container mx-auto px-6 py-8 gap-8 flex justify-between items-center">
+      <div>
+        <div class="w-36" >
+          <NavigationLogo />
+        </div>
+        <UiTypographyP class="mt-2">&copy; Added Digital Solutions, {{new Date().getFullYear()}}</UiTypographyP>
       </div>
-      <UiTypographyP>&copy; Added Digital Solutions, 2023</UiTypographyP>
+
+      <div class="flex items-center gap-4">
+        <NuxtLink to="" class="w-10 h-10 rounded-md bg-white bg-opacity-10 grid place-items-center">
+        <img src="@/assets/images/socials/instagram.svg" class="w-6 h-6" alt="">
+        </NuxtLink>
+        <NuxtLink to="" class="w-10 h-10 rounded-md bg-white bg-opacity-10 grid place-items-center">
+          <img src="@/assets/images/socials/whatsapp.svg" class="w-6 h-6" alt="">
+        </NuxtLink>
+        <NuxtLink to="" class="w-10 h-10 rounded-md bg-white bg-opacity-10 grid place-items-center">
+          <img src="@/assets/images/socials/twitter.svg" class="w-6 h-6" alt="">
+        </NuxtLink>
+        <NuxtLink to="" class="w-10 h-10 rounded-md bg-white bg-opacity-10 grid place-items-center">
+          <img src="@/assets/images/socials/facebook.svg" class="w-6 h-6" alt="">
+        </NuxtLink>
+      </div>
+
     </div>
   </footer>
 </template>
