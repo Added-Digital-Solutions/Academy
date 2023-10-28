47f61d713325b70962f8ec40fd59b67debb966d0
Qaudri <aquadirmuhammad@gmail.com>
2023-10-28 15:55:58 +0100

    new sections added to course details page

--- pages/courses/[course].vue
+++ pages/courses/[course].vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="bg-primary text-white min-h-screen">
+  <main  class="bg-primary text-white min-h-screen">
     <div class="max-w-7xl px-6 py-24 flex-col md:flex-row flex gap-8 md:gap-12 lg:gap-20 items-center justify-between mx-auto">
       <div class="w-full md:w-1/2 grid gap-6">
         <UiTypographyH1>{{$route.params.course}}</UiTypographyH1>
@@ -18,38 +18,82 @@
   </main>
 
   <section class="py-20 px-6 max-w-7xl mx-auto flex gap-8 text-accent text-center">
-    <ul class="grid gap-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-      <li>
+    <ul class="grid gap-8 grid-cols-2 mx-auto md:grid-cols-3 lg:grid-cols-6">
+      <li class="flex flex-col gap-2">
         <img src="@/assets/images/duration.svg" class="w-10 h-10 mx-auto" alt="">
         <UiTypographyP>Duration</UiTypographyP>
-        <h2 class="text-2xl font-semibold mt-2">3 months</h2>
+        <h2 class="text-2xl font-semibold">3 months</h2>
       </li>
-      <li>
+      <li class="flex flex-col gap-2">
         <img src="@/assets/images/calendar.svg" class="w-10 h-10 mx-auto" alt="">
         <UiTypographyP>Start Date</UiTypographyP>
-        <h2 class="text-2xl font-semibold mt-2">December 1st, 2023</h2>
+        <h2 class="text-2xl font-semibold">December 1st, 2023</h2>
       </li>
-      <li>
+      <li class="flex flex-col gap-2">
         <img src="@/assets/images/level.svg" class="w-10 h-10 mx-auto" alt="">
         <UiTypographyP>Level</UiTypographyP>
-        <h2 class="text-2xl font-semibold mt-2">Beginner</h2>
+        <h2 class="text-2xl font-semibold">Beginner</h2>
       </li>
-      <li>
+      <li class="flex flex-col gap-2">
         <img src="@/assets/images/list.svg" class="w-10 h-10 mx-auto" alt="">
         <UiTypographyP>Prerequisites</UiTypographyP>
-        <h2 class="text-2xl font-semibold mt-2">A laptop</h2>
+        <h2 class="text-2xl font-semibold">A laptop</h2>
       </li>
-      <li>
+      <li class="flex flex-col gap-2">
         <img src="@/assets/images/projects.svg" class="w-10 h-10 mx-auto" alt="">
         <UiTypographyP>Projects</UiTypographyP>
-        <h2 class="text-2xl font-semibold mt-2">4 projects</h2>
+        <h2 class="text-2xl font-semibold">4 projects</h2>
       </li>
-      <li>
+      <li class="flex flex-col gap-2">
         <img src="@/assets/images/card.svg" class="w-10 h-10 mx-auto" alt="">
         <UiTypographyP>Cost</UiTypographyP>
-        <h2 class="text-2xl font-semibold mt-2">₦500,000</h2>
+        <h2 class="text-2xl font-semibold">₦500,000</h2>
       </li>
     </ul>
+  </section>
+
+  <section class="bg-light text-accent text-center">
+    <div class="py-20 px-6 max-w-7xl">
+      <UiTypographyH2>come with expectations<span class="text-primary">.</span></UiTypographyH2>
+
+      <ul class="grid gap-8 md:grid-cols-2 lg:grid-cols-3 mt-12">
+        <li class="flex flex-col gap-2 w-4/5 mx-auto">
+          <img src="@/assets/images/teacher.svg" class="w-12 h-12 mx-auto" alt="">
+          <UiTypographyP class="font-semibold">Experienced Teachers</UiTypographyP>
+          <UiTypographyP class="text-2xl">You'll be taught and mentored by some of the best teachers in the industry.</UiTypographyP>
+        </li>
+        <li class="flex flex-col gap-2 w-4/5 mx-auto">
+          <img src="@/assets/images/projects.svg" class="w-12 h-12 mx-auto" alt="">
+          <UiTypographyP class="font-semibold">Hands-on</UiTypographyP>
+          <UiTypographyP class="text-2xl">You will be taught & mentored by Industry expert.</UiTypographyP>
+        </li>
+        <li class="flex flex-col gap-2 w-4/5 mx-auto">
+          <img src="@/assets/images/certificate.svg" class="w-12 h-12 mx-auto" alt="">
+          <UiTypographyP class="font-semibold">Certificate</UiTypographyP>
+          <UiTypographyP class="text-2xl">Get your very own physical certificate to prove your participation.</UiTypographyP>
+        </li>
+        <li class="flex flex-col gap-2 w-4/5 mx-auto">
+          <img src="@/assets/images/support.svg" class="w-12 h-12 mx-auto" alt="">
+          <UiTypographyP class="font-semibold">Support System</UiTypographyP>
+          <UiTypographyP class="text-2xl">Become part of an ever-growing, supportive community of like minds.</UiTypographyP>
+        </li>
+        <li class="flex flex-col gap-2 w-4/5 mx-auto">
+          <img src="@/assets/images/building.svg" class="w-12 h-12 mx-auto" alt="">
+          <UiTypographyP class="font-semibold">Conducive environment</UiTypographyP>
+          <UiTypographyP class="text-2xl">Our classes and the environment is serene and conducive for learning.</UiTypographyP>
+        </li>
+        <li class="flex flex-col gap-2 w-4/5 mx-auto">
+          <img src="@/assets/images/building.svg" class="w-12 h-12 mx-auto" alt="">
+          <UiTypographyP class="font-semibold">Souvenirs</UiTypographyP>
+          <UiTypographyP class="text-2xl">You will get a free t-shirt. This is to make you feel right at home, cause why not?!</UiTypographyP>
+        </li>
+      </ul>
+    </div>
+  </section>
+
+  <section class="py-20 px-6 max-w-7xl mx-auto text-accent text-center">
+    <UiTypographyH2>success stories<span class="text-primary">.</span></UiTypographyH2>
+
   </section>
 </template>
 <style>
