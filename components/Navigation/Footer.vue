b8cc653a73986f6a9bcfaeeb6085b74d20f36dad
Quadir <aquadirmuhammad@gmail.com>
2023-11-29 22:04:16 +0100

    footer hyperlinks target updated

--- components/Navigation/Footer.vue
+++ components/Navigation/Footer.vue
@@ -3,16 +3,16 @@
     <div class="max-w-7xl container mx-auto px-6 p-8 md:flex justify-between items-center">
       <div class="" >
         <div class="flex gap-4 items-center">
-        <NuxtLink to="https://instagram.com/addeddigitalsolutions?igshid=MzRlODBiNWFlZA==" class="border-[3px] border-accent rounded-md w-10 h-10 grid place-items-center">
+        <NuxtLink target="_blank" to="https://instagram.com/addeddigitalsolutions?igshid=MzRlODBiNWFlZA==" class="border-[3px] border-accent rounded-md w-10 h-10 grid place-items-center">
           <img src="@/assets/images/insta.svg" class="w-6 h-6" alt="">
         </NuxtLink>
-        <NuxtLink class="border-[3px] border-accent rounded-md w-10 h-10 grid place-items-center">
+        <NuxtLink target="_blank" class="border-[3px] border-accent rounded-md w-10 h-10 grid place-items-center">
           <img src="@/assets/images/facebook.svg" class="w-6 h-6" alt="">
         </NuxtLink>
-        <NuxtLink to="https://twitter.com/addeddigital?t=OZkFn-mOMALUjGTTFOgQeg&s=09" class="border-[3px] border-accent rounded-md w-10 h-10 grid place-items-center">
+        <NuxtLink target="_blank" to="https://twitter.com/addeddigital?t=OZkFn-mOMALUjGTTFOgQeg&s=09" class="border-[3px] border-accent rounded-md w-10 h-10 grid place-items-center">
           <img src="@/assets/images/twitter.svg" class="w-6 h-6" alt="">
         </NuxtLink>
-        <NuxtLink class="border-[3px] border-accent rounded-md w-10 h-10 grid place-items-center">
+        <NuxtLink target="_blank" class="border-[3px] border-accent rounded-md w-10 h-10 grid place-items-center">
           <img src="@/assets/images/linkedin.svg" class="w-6 h-6" alt="">
         </NuxtLink>
         </div>
